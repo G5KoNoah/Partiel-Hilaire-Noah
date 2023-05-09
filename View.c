3995ef2f4cc91d5cbf3e987fa729b6e9b49742c5
G5KoNoah <117288019+G5KoNoah@users.noreply.github.com>
2023-05-09 16:40:30 +0200

    Play

--- View.c
+++ View.c
@@ -1,7 +1,15 @@
 int choiceCase(){
-  // l'utilisateur choisit sa case sur l'interface 
+  // l'utilisateur choisit sa case sur l'interface et renvoie le numéro de la case
 }
 
 int choiceNumber(){
-  // l'utilisateur choisit le nombre qu'il veut mettre sur la case qu'il a choisit
+  // l'utilisateur choisit le nombre qu'il veut mettre sur la case qu'il a choisit et renvoie ce chiffre
+}
+
+int play(){
+  // l'utilisateur clique sur lancer le jeu et renvoie 1 si oui
+}
+
+int replay(){
+  // l'utilisateur clique sur relancer le jeu et renvoie 1 si oui
 }
