a4ea3e3cfad5e00a9cbeafad3da3f2928936719d
G5KoNoah <117288019+G5KoNoah@users.noreply.github.com>
2023-05-09 16:38:35 +0200

    Choice

--- View.c
+++ View.c
@@ -1,1 +1,7 @@
+int choiceCase(){
+  // l'utilisateur choisit sa case sur l'interface 
+}
 
+int choiceNumber(){
+  // l'utilisateur choisit le nombre qu'il veut mettre sur la case qu'il a choisit
+}
