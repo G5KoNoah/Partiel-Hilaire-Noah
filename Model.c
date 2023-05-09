0ad5556d1fc15704e5e30e5e0110929a59ae7a26
G5KoNoah <117288019+G5KoNoah@users.noreply.github.com>
2023-05-09 16:48:22 +0200

    Verification choice and win

---
+++ Model.c
@@ -0,0 +1,11 @@
+int main(){
+  //fonction principale qui va appeler les autres fonctions
+}
+
+int verifChoice(){
+  //verifie si le choix de l'utilisateur rentre dans les règles du jeu (ex: un seul même chiffre par ligne) et renvoie 1 si le chiffre ne peut être placé
+}
+
+int verifWIn(){
+  //verifie si tous les chiffres de la grille sont complétés et renvoie 1 si oui
+}
